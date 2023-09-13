5e4fb4ce5aff507cdc98f265bddf94363150e548
Jiggafreewill <jiggafreewill@gmail.com>
2023-09-12 22:04:03 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
+    /* Call print_times_table with different values to test it */
     print_times_table(3);
     _putchar('\n');
     print_times_table(5);
@@ -14,5 +15,6 @@ int main(void)
     print_times_table(98);
     _putchar('\n');
     print_times_table(12);
+
     return (0);
 }
